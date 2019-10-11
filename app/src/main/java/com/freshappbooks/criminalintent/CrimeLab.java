b5622717ff83679695ea1c63348e7fd019432b1f
vjmarcus <vjmarcus1@gmail.com>
2019-10-11 10:17:02 +0300

    Add some new classes

--- app/src/main/java/com/freshappbooks/criminalintent/CrimeLab.java
+++ app/src/main/java/com/freshappbooks/criminalintent/CrimeLab.java
@@ -2,11 +2,36 @@ package com.freshappbooks.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {  //Singleton class
 
     private static CrimeLab crimeLab;
+    private List <Crime> crimes;
 
     private CrimeLab(Context context){
+        crimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            crimes.add(crime);
+        }
+    }
+
+    public List <Crime> getCrimes() {
+        return crimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime: crimes){
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
     }
 
     public static CrimeLab get(Context context) {
---
+++ app/src/main/java/com/freshappbooks/criminalintent/CrimeListFragment.java
@@ -0,0 +1,6 @@
+package com.freshappbooks.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+}
