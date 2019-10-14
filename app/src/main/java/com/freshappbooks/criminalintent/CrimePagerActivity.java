6642329ef7e2ca54951133fcea94d017f712bb7e
vjmarcus <vjmarcus1@gmail.com>
2019-10-14 19:55:38 +0300

    Update CrimePagerActivity

--- app/src/main/java/com/freshappbooks/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/freshappbooks/criminalintent/CrimePagerActivity.java
@@ -2,14 +2,42 @@ package com.freshappbooks.criminalintent;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
 
 public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private List<Crime> crimes;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        viewPager = findViewById(R.id.crime_view_pager);
+        crimes = CrimeLab.get(this).getCrimes();
+        FragmentManager manager = getSupportFragmentManager();
+        viewPager.setAdapter(new FragmentStatePagerAdapter(manager) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = crimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return crimes.size();
+            }
+        });
     }
 }
