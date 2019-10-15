c28660c46af5879f2bbf57a08ad165bab0532ea4
vjmarcus <vjmarcus1@gmail.com>
2019-10-15 12:32:00 +0300

    Update CrimePagerActivity

--- app/src/main/java/com/freshappbooks/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/freshappbooks/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.freshappbooks.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,16 +9,24 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
 
+    private static final String EXTRA_CRIME_ID = "asdflsakdjfsegsdddkdkdkkdkd";
     private ViewPager viewPager;
     private List<Crime> crimes;
+
+    public static Intent newIntent(Context context, UUID crimeId) {
+        Intent intent = new Intent(context, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
